0864a70d83b907eaa0ea0555561b71730bb6c7d5
shimotmk <shimotmk1104@gmail.com>
2021-05-24 16:26:49 +0900

    add：フォーム送信時にユーザー登録を行う機能追加

--- README.md
+++ README.md
@@ -11,3 +11,8 @@
 https://updraftplus.com/wp-cli-updraftplus-documentation/
 https://wp-cli.org/ja/
 https://www.php.net/manual/ja/function.exec.php
+
+## 初期値やデフォルトの値を変更したい場合
+1.変更を加える
+2.バックアップを取る
+3.生成したバックアップファイルのnonceを見てプラグインを書き換える
--- vk-trial-auto-supporter.php
+++ vk-trial-auto-supporter.php
@@ -9,69 +9,93 @@
  * Text Domain: vk-trial-auto-supporter
  */
 
+
 /****
  * 復元する wp-cliコマンド
  * 03f928d85c1cはnonceでバックアップを作ると自動で作られる識別子 
  * 管理画面から確認する
-
+ * 毎日一度行うコマンド類
  */
 function vk_wp_cli() {
-	// exec('wp updraftplus restore 03f928d85c1c');
+  //テーマをアップデートする
+  // exec( 'wp theme update --all' );
+
+  //プラグインをアップデートする
+  //exec( 'wp plugin update --all' );
+
+  // プラグインやテーマのアップデートも復元されてしまうので、データベースのみ復元する
+	// exec('wp updraftplus restore 03f928d85c1c --components="db"');
+
+  //仕様版ユーザーのパスワードを変更する 仕様版ユーザーID '2'を設定
+  // $password = wp_generate_password( 12, true );
+  // wp_set_password( $password, 2 );
 }
 add_action( 'vk_trial_cli', 'vk_wp_cli' );
 
-/***
- * 定期的に実行する用テスト
- * これでは誰かがサイトに訪れた時にしか動かないので、お問い合わせを頂いた時点でのユーザー登録がなくなる可能性がある
- */
-function cron_add_1min( $schedules ) {
-  $schedules['1min'] = array(
-    'interval' => 1*60, //インターバルの時間
-    'display' => __( 'Once every one minutes' )
-  );
-  return $schedules;
-}
-add_filter( 'cron_schedules', 'cron_add_1min' );
-
-if ( ! wp_next_scheduled( 'my_task_hook' ) ) {
-  date_default_timezone_set('Asia/Tokyo');
-  //スタートする時間 リリースする時に02:00:00にすればOK
-  $start_time = strtotime("2020/5/21 14:07:00");
-  wp_schedule_event( $start_time, '1min', 'my_task_hook' );
-}
-
-function do_stuff(){
-  // exec('wp post create --post_title=CLI投稿テスト --post_status=draft --porcelain');
-}
-add_action( 'my_task_hook', 'do_stuff' );
-
 
 /**
- * 管理画面 
- * */
-function register_custom_settings() {
-  register_setting('original-field-vk-trial', 'vk_trial_auto_supporter_nonce');
-  register_setting('original-field-vk-trial', 'vk_trial_auto_supporter_time');
-}
-add_action('admin_init', 'register_custom_settings');
+ * フォーム送信時にユーザー登録をおこなう
+ */
+function wpcf7_insert_user($contact_form, &$abort, $submission) {
+	//送信情報を取得
+	$submission = WPCF7_Submission::get_instance();
+	if($submission) {
+    //送信された情報を取得
+		$formdata = $submission->get_posted_data();
+    //パスワードを自動生成する
+    $password = wp_generate_password( 12, true );
+    
+    //ユーザーデータをセット
+		$userdata = array(
+		    'user_login'    => $formdata['your-login'], //ユーザー名
+		    'user_email'    => $formdata['your-email'], //メールアドレス
+		    'user_pass'     => $password, //パスワード
+        'role'          => 'trial_user' //ユーザー権限
+		);
+		//ユーザーデータ登録
+		$user_id = wp_insert_user($userdata);
+    /**
+     * ユーザー登録に成功した場合
+     * $abort = false;
+     * 参考：https://wordpress.org/support/topic/what-is-the-right-way-of-stopping-cf7-from-posting-using-wpcf7_before_send_mail/
+     */
+    if ( ! is_wp_error( $user_id ) ) {
+      $abort = false;
+      /**
+       * 自動返信メールで生成されたパスワードを渡す
+       */
+      // 管理者宛メール
+      $mail = $contact_form->prop( 'mail' ); 
+      // 自動返信メール
+      $mail2 = $contact_form->prop( 'mail_2' ); 
 
-function vk_trial_setting_menu() {
-	$custom_page = add_options_page(
-		'VK Trial Auto Supporter',  // Name of page
-		'VK Trial Auto Supporter',  // Label in menu
-		'edit_theme_options',       // Capability required　このメニューページを閲覧・使用するために最低限必要なユーザーレベルまたはユーザーの種類と権限。
-		'vk_trial_options',     // ユニークなこのサブメニューページの識別子スラッグ
-		function() { include(dirname(__FILE__) . '/admin/vk_trial_options_manage.php'); },  // 管理画面のページの定義関数
-	);
-	if ( ! $custom_page ) {
-		return;
+      // パスワードの文字列を変更する
+      $mail['body'] = str_replace("generate_password", $password, $mail['body']);
+      $mail2['body'] = str_replace("generate_password", $password, $mail2['body']);
+
+      // set mail property with changed value(s)
+      $contact_form->set_properties( array( 'mail' => $mail ) );
+      $contact_form->set_properties( array( 'mail_2' => $mail2 ) );
+
+    } else {
+      $abort = true;
+    }
+    return $contact_form;
 	}
 }
-add_action( 'admin_menu', 'vk_trial_setting_menu' );
+add_action('wpcf7_before_send_mail', 'wpcf7_insert_user',10, 3);
 
-function my_katawara_header_before() {
-  echo "<p>ゲットオプション内容</p>";
-  $vk_trial_auto_supporter_nonce = get_option('vk_trial_auto_supporter_nonce', 'ここ');
-  echo $sitename_main;
-}
-add_action('katawara_header_before', 'my_katawara_header_before');
+
+/****
+ * フォーム送信時のメッセージの出力を加工する
+ * 参考：https://stackoverflow.com/questions/64694718/how-to-dynamically-change-contact-form-7-submission-display-message
+ */
+function filter_wpcf7_display_message( $message, $status ) { 
+  if ('mail_sent_ok' == $status) {
+    $message = "申請ありがとうございました。送信頂いたメールアドレスにログイン情報をお送りさせていただいたのでご確認お願いします";
+  } else {
+    $message = "フォームの送信に失敗しました。";
+  }
+  return $message; 
+}; 
+add_filter( 'wpcf7_display_message', 'filter_wpcf7_display_message', 10, 2 ); 
