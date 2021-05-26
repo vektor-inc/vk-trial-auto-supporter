ff65425c4d78ec343506e080c0286a4b3f26a29b
shimotmk <shimotmk1104@gmail.com>
2021-05-26 15:01:55 +0900

    フォーム申請ページ用の投稿タイプを作成

--- vk-trial-auto-supporter.php
+++ vk-trial-auto-supporter.php
@@ -12,7 +12,6 @@
 /**
  *  深夜２時以降にユーザーからアクセスがあった時に１日１回実行する関数
  *  アクセスが無ければ実行されない
- *  https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/wp_schedule_event
  */
 function vk_trial_auto_function() {
   /**
@@ -31,8 +30,7 @@ function vk_trial_auto_function() {
    * 復元するコマンド
    * https://updraftplus.com/wp-cli-updraftplus-documentation/
    * 
-   * 03f928d85c1cはnonceでバックアップを作ると自動で作られる識別子 
-   * 管理画面から確認する
+   * 03f928d85c1cはnonceでバックアップを作ると自動で作られる識別子 管理画面から確認する
    * データベースのみ復元する
    */
   // exec('wp updraftplus restore 03f928d85c1c --components="db"');
@@ -42,7 +40,7 @@ function vk_trial_auto_function() {
    * 試用版ユーザーID '2'を設定
    */
   $password = wp_generate_password( 12, true );
-  wp_set_password( $password, 3 );
+  wp_set_password( $password, 2 );
 
   /**
    * 自動返信メールで試用ユーザーにパスワードを送るためにDBに保存
@@ -51,37 +49,38 @@ function vk_trial_auto_function() {
     add_option( 'vektor_guest_password', $password );
   }
   update_option( 'vektor_guest_password', $password );
+
 }
 add_action ( 'vk_trial_form_auto_cron', 'vk_trial_auto_function' );
 
 /**
  *  検証用インターバル設定関数
  *  本番は１日おきなのでデフォルトのdailyを使用
- *  vk_trial_form_intervalの関数は不要
+ *  vk_trial_intervalの関数は不要
  */
-function vk_trial_form_interval( $schedules ) {
-	// 60秒毎を追加
-	$schedules['60sec'] = array(
+function vk_trial_interval( $schedules ) {
+	// 300秒毎を追加
+	$schedules['300sec'] = array(
 		'interval' => 300,
-		'display' => 'every 60 seconds'
+		'display' => 'every 300 seconds'
 	);
 	return $schedules;
 }
-add_filter( 'cron_schedules', 'vk_trial_form_interval' ); 
+add_filter( 'cron_schedules', 'vk_trial_interval' ); 
 
 /**
  * イベントの実行
- * 本番では 
- * strtotime('2021-05-25 02:00:00')を指定の時刻からスタート
- * 60secをdailyに変える
+ * 本番ではstrtotime('2021-05-25 02:00:00')を変更し指定の時刻からスタート
+ * 300secをdailyに変える
  */
 if ( !wp_next_scheduled( 'vk_trial_form_auto_cron' ) ) { 
   date_default_timezone_set( 'Asia/Tokyo' );
-  wp_schedule_event( strtotime( '2021-05-25 13:55:00' ), '60sec', 'vk_trial_form_auto_cron' );
+  wp_schedule_event( strtotime( '2021-05-25 02:00:00' ), '300sec', 'vk_trial_form_auto_cron' );
 }
 
 /**
  * 自動返信でパスワードを送る
+ * 参考：https://stackoverflow.com/questions/28857693/wordpress-overwrite-contact-form-mail-2-body
  */
 function wpcf7_post_password ( $contact_form, &$abort, $submission ) {
   /**
@@ -123,23 +122,21 @@ add_action( 'wpcf7_before_send_mail', 'wpcf7_post_password', 10, 3 );
 /**
  * リセット前１時間前くらいからはフォーム申請を停止する
  * https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/wp_schedule_event
- * 本番では
- * 1から2時
+ * 本番では 1から2時
  */
-// function filter_wpcf7_acceptance( $true ) { 
-//   $nowdate = date_i18n('H'); 
-//   if ( $nowdate >= 12 && $nowdate < 14 ) {
-//     $true = false;
-// 	} else {
-//     $true = true;
-// 	} 
-//   return $true; 
-// }; 
-// add_filter( 'wpcf7_acceptance', 'filter_wpcf7_acceptance' );
+function filter_wpcf7_acceptance( $true ) { 
+  $nowdate = date_i18n('H'); 
+  if ( 1 <= $nowdate && $nowdate < 2 ) {
+    $true = false;
+	} else {
+    $true = true;
+	} 
+  return $true; 
+}; 
+add_filter( 'wpcf7_acceptance', 'filter_wpcf7_acceptance' );
 
 /**
- * フォーム送信時のメッセージの出力を加工する
- * 参考：https://stackoverflow.com/questions/64694718/how-to-dynamically-change-contact-form-7-submission-display-message
+ * フォームの申請を止めるのでフォーム送信時のメッセージの出力を加工する
  */
 function filter_wpcf7_display_message( $message, $status ) { 
   if ( 'mail_sent_ok' == $status ) {
@@ -150,3 +147,28 @@ function filter_wpcf7_display_message( $message, $status ) {
   return $message; 
 }; 
 add_filter( 'wpcf7_display_message', 'filter_wpcf7_display_message', 10, 2 ); 
+
+/**
+ * 管理者用投稿タイプ
+ * 試用版ユーザーの申請フォームページのため
+ */
+function add_admin_only_post_type_manage() {
+  register_post_type(
+    'vk_trial',
+    array(
+      'labels'          => array(
+        'name'          => '管理者用投稿タイプ',
+      ),
+      'public'          => true,
+      'show_ui'         => true,
+      'show_in_menu'    => true,
+      'show_in_rest'    => true,
+      'menu_position'   => 100,
+      'capability_type' => array( 'post_type_manage', 'post_type_manages' ),
+      'map_meta_cap'    => true,
+      'menu_icon'       => 'dashicons-admin-generic',
+      'supports'        => array( 'title' , 'editor' ),
+    )
+  );
+}
+add_action( 'init', 'add_admin_only_post_type_manage' );
