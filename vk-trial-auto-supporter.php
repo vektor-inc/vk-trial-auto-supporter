361e4a0ce0973efd8afc787f3de03bb06f73f35f
shimotmk <shimotmk1104@gmail.com>
2021-05-26 15:29:40 +0900

    clean: インデント,スペースの調整

--- vk-trial-auto-supporter.php
+++ vk-trial-auto-supporter.php
@@ -14,41 +14,41 @@
  *  アクセスが無ければ実行されない
  */
 function vk_trial_auto_function() {
-  /**
-   * テーマをアップデートする
-   * https://developer.wordpress.org/cli/commands/theme/update/
-   */
-  exec( 'wp theme update --all' );
+	/**
+	 * テーマをアップデートする
+	 * https://developer.wordpress.org/cli/commands/theme/update/
+	 */
+	exec( 'wp theme update --all' );
 
-  /**
-   * プラグインをアップデートする
-   * https://developer.wordpress.org/cli/commands/plugin/update/
-   */
-  exec( 'wp plugin update --all' );
+	/**
+	 * プラグインをアップデートする
+	 * https://developer.wordpress.org/cli/commands/plugin/update/
+	 */
+	exec( 'wp plugin update --all' );
 
-  /**
-   * 復元するコマンド
-   * https://updraftplus.com/wp-cli-updraftplus-documentation/
-   * 
-   * 03f928d85c1cはnonceでバックアップを作ると自動で作られる識別子 管理画面から確認する
-   * データベースのみ復元する
-   */
-  // exec('wp updraftplus restore 03f928d85c1c --components="db"');
+	/**
+	 * 復元するコマンド
+	 * https://updraftplus.com/wp-cli-updraftplus-documentation/
+	 * 
+	 * 03f928d85c1cはnonceでバックアップを作ると自動で作られる識別子 管理画面から確認する
+	 * データベースのみ復元する
+	 */
+	// exec('wp updraftplus restore 03f928d85c1c --components="db"');
 
-  /**
-   * 試用版ユーザーのパスワードを変更する 
-   * 試用版ユーザーID '2'を設定
-   */
-  $password = wp_generate_password( 12, true );
-  wp_set_password( $password, 2 );
+	/**
+	 * 試用版ユーザーのパスワードを変更する 
+	 * 試用版ユーザーID '2'を設定
+	 */
+	$password = wp_generate_password( 12, true );
+	wp_set_password( $password, 2 );
 
-  /**
-   * 自動返信メールで試用ユーザーにパスワードを送るためにDBに保存
-   */
-  if ( ! get_option( 'vektor_guest_password' ) ) {
-    add_option( 'vektor_guest_password', $password );
-  }
-  update_option( 'vektor_guest_password', $password );
+	/**
+	 * 自動返信メールで試用ユーザーにパスワードを送るためにDBに保存
+	 */
+	if ( ! get_option( 'vektor_guest_password' ) ) {
+		add_option( 'vektor_guest_password', $password );
+	}
+	update_option( 'vektor_guest_password', $password );
 
 }
 add_action ( 'vk_trial_form_auto_cron', 'vk_trial_auto_function' );
@@ -59,23 +59,22 @@ add_action ( 'vk_trial_form_auto_cron', 'vk_trial_auto_function' );
  *  vk_trial_intervalの関数は不要
  */
 function vk_trial_interval( $schedules ) {
-	// 300秒毎を追加
 	$schedules['300sec'] = array(
 		'interval' => 300,
 		'display' => 'every 300 seconds'
 	);
 	return $schedules;
 }
-add_filter( 'cron_schedules', 'vk_trial_interval' ); 
+add_filter( 'cron_schedules', 'vk_trial_interval' );
 
 /**
  * イベントの実行
  * 本番ではstrtotime('2021-05-25 02:00:00')を変更し指定の時刻からスタート
  * 300secをdailyに変える
  */
-if ( !wp_next_scheduled( 'vk_trial_form_auto_cron' ) ) { 
-  date_default_timezone_set( 'Asia/Tokyo' );
-  wp_schedule_event( strtotime( '2021-05-25 02:00:00' ), '300sec', 'vk_trial_form_auto_cron' );
+if ( !wp_next_scheduled( 'vk_trial_form_auto_cron' ) ) {
+	date_default_timezone_set( 'Asia/Tokyo' );
+	wp_schedule_event( strtotime( '2021-05-25 02:00:00' ), '300sec', 'vk_trial_form_auto_cron' );
 }
 
 /**
@@ -83,39 +82,39 @@ if ( !wp_next_scheduled( 'vk_trial_form_auto_cron' ) ) {
  * 参考：https://stackoverflow.com/questions/28857693/wordpress-overwrite-contact-form-mail-2-body
  */
 function wpcf7_post_password ( $contact_form, &$abort, $submission ) {
-  /**
-   * 送信情報を取得
-   */
+	/**
+	 * 送信情報を取得
+	 */
 	$submission = WPCF7_Submission::get_instance();
 	if( $submission ) {
-    /**
-     * 送信された情報を取得
-     */
+		/**
+		 * 送信された情報を取得
+		 */
 		$formdata = $submission->get_posted_data();
-    
-    /**
-     * wp-cronで生成されたパスワード
-     */
-    $password = get_option( 'vektor_guest_password' );
-    
-    /**
-     * mailは管理者宛のメール
-     * mail_2は自動返信メール
-     */
-    $mail  = $contact_form->prop( 'mail' ); 
-    $mail2 = $contact_form->prop( 'mail_2' ); 
 
-    /**
-     * メール文章のgenerate_passwordの文字列を現在の設定されているパスワードに変更する
-     */
-    $mail['body']  = str_replace( "generate_password", $password, $mail['body'] );
-    $mail2['body'] = str_replace( "generate_password", $password, $mail2['body'] );
+		/**
+		 * wp-cronで生成されたパスワード
+		 */
+		$password = get_option( 'vektor_guest_password' );
 
-    $contact_form->set_properties( array( 'mail' => $mail ) );
-    $contact_form->set_properties( array( 'mail_2' => $mail2 ) );
+		/**
+		 * mailは管理者宛のメール
+		 * mail_2は自動返信メール
+		 */
+		$mail  = $contact_form->prop( 'mail' );
+		$mail2 = $contact_form->prop( 'mail_2' );
 
-    return $contact_form;
-  }
+		/**
+		 * メール文章のgenerate_passwordの文字列を現在の設定されているパスワードに変更する
+		 */
+		$mail['body']  = str_replace( "generate_password", $password, $mail['body'] );
+		$mail2['body'] = str_replace( "generate_password", $password, $mail2['body'] );
+
+		$contact_form->set_properties( array( 'mail' => $mail ) );
+		$contact_form->set_properties( array( 'mail_2' => $mail2 ) );
+
+		return $contact_form;
+	}
 }
 add_action( 'wpcf7_before_send_mail', 'wpcf7_post_password', 10, 3 );
 
@@ -125,50 +124,50 @@ add_action( 'wpcf7_before_send_mail', 'wpcf7_post_password', 10, 3 );
  * 本番では 1から2時
  */
 function filter_wpcf7_acceptance( $true ) { 
-  $nowdate = date_i18n('H'); 
-  if ( 1 <= $nowdate && $nowdate < 2 ) {
-    $true = false;
+	$nowdate = date_i18n('H'); 
+	if ( 1 <= $nowdate && $nowdate < 2 ) {
+		$true = false;
 	} else {
-    $true = true;
-	} 
-  return $true; 
-}; 
+		$true = true;
+	}
+	return $true;
+};
 add_filter( 'wpcf7_acceptance', 'filter_wpcf7_acceptance' );
 
 /**
  * フォームの申請を止めるのでフォーム送信時のメッセージの出力を加工する
  */
 function filter_wpcf7_display_message( $message, $status ) { 
-  if ( 'mail_sent_ok' == $status ) {
-    $message = "申請ありがとうございました。送信頂いたメールアドレスにログイン情報をお送りさせていただいたのでご確認お願いします";
-  } else {
-    $message = "フォームの送信に失敗しました。";
-  }
-  return $message; 
-}; 
-add_filter( 'wpcf7_display_message', 'filter_wpcf7_display_message', 10, 2 ); 
+	if ( 'mail_sent_ok' == $status ) {
+		$message = "申請ありがとうございました。送信頂いたメールアドレスにログイン情報をお送りさせていただいたのでご確認お願いします";
+	} else {
+		$message = "フォームの送信に失敗しました。";
+	}
+	return $message;
+};
+add_filter( 'wpcf7_display_message', 'filter_wpcf7_display_message', 10, 2 );
 
 /**
  * 管理者用投稿タイプ
  * 試用版ユーザーの申請フォームページのため
  */
 function add_admin_only_post_type_manage() {
-  register_post_type(
-    'vk_trial',
-    array(
-      'labels'          => array(
-        'name'          => '管理者用投稿タイプ',
-      ),
-      'public'          => true,
-      'show_ui'         => true,
-      'show_in_menu'    => true,
-      'show_in_rest'    => true,
-      'menu_position'   => 100,
-      'capability_type' => array( 'post_type_manage', 'post_type_manages' ),
-      'map_meta_cap'    => true,
-      'menu_icon'       => 'dashicons-admin-generic',
-      'supports'        => array( 'title' , 'editor' ),
-    )
-  );
+	register_post_type(
+		'vk_trial',
+		array(
+			'labels'          => array(
+				'name'          => '管理者用投稿タイプ',
+			),
+			'public'          => true,
+			'show_ui'         => true,
+			'show_in_menu'    => true,
+			'show_in_rest'    => true,
+			'menu_position'   => 100,
+			'capability_type' => array( 'post_type_manage', 'post_type_manages' ),
+			'map_meta_cap'    => true,
+			'menu_icon'       => 'dashicons-admin-generic',
+			'supports'        => array( 'title' , 'editor' ),
+		)
+	);
 }
 add_action( 'init', 'add_admin_only_post_type_manage' );
