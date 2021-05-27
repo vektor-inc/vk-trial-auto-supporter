4d15374d0becdf43fee4c2ce35b5ef960c98fd75
shimotmk <shimotmk1104@gmail.com>
2021-05-27 14:33:27 +0900

    update:アップデートが正常に行われたかチェックする記述を追加

--- vk-trial-auto-supporter.php
+++ vk-trial-auto-supporter.php
@@ -18,13 +18,39 @@ function vk_trial_auto_function() {
 	 * テーマをアップデートする
 	 * https://developer.wordpress.org/cli/commands/theme/update/
 	 */
-	exec( 'wp theme update --all' );
+	exec( 'wp theme update --all', $output, $return_var );
+  if ( $return_var !== 0 ){
+    $result = 'false';
+  } else {
+    $result = 'true';
+  }
+
+  /**
+	 * テーマアップデートが正常に終わったかどうか
+	 */
+	if ( ! get_option( 'vk_theme_update' ) ) {
+		add_option( 'vk_theme_update', $result );
+	}
+	update_option( 'vk_theme_update', $result );
 
 	/**
 	 * プラグインをアップデートする
 	 * https://developer.wordpress.org/cli/commands/plugin/update/
 	 */
-	exec( 'wp plugin update --all' );
+	exec( 'wp plugin update --all', $output, $return_var );
+	if ( $return_var !== 0 ){
+		$result = 'false';
+	} else {
+		$result = 'true';
+	}
+
+  /**
+	 * テーマアップデートが正常に終わったかどうか
+	 */
+	if ( ! get_option( 'vk_plugin_update' ) ) {
+		add_option( 'vk_plugin_update', $result );
+	}
+	update_option( 'vk_plugin_update', $result );
 
 	/**
 	 * 復元するコマンド
@@ -33,7 +59,20 @@ function vk_trial_auto_function() {
 	 * 03f928d85c1cはnonceでバックアップを作ると自動で作られる識別子 管理画面から確認する
 	 * データベースのみ復元する
 	 */
-	// exec('wp updraftplus restore 03f928d85c1c --components="db"');
+	// exec('wp updraftplus restore 03f928d85c1c --components="db"' , $output, $return_var);
+	if ( $return_var !== 0 ){
+		$result = 'false';
+	} else {
+		$result = 'true';
+	}
+
+  /**
+	 * 復元が正常に終わったかどうか
+	 */
+	if ( ! get_option( 'vk_updraftplus_restore' ) ) {
+		add_option( 'vk_updraftplus_restore', $result );
+	}
+	update_option( 'vk_updraftplus_restore', $result );
 
 	/**
 	 * 試用版ユーザーのパスワードを変更する 
@@ -49,6 +88,24 @@ function vk_trial_auto_function() {
 		add_option( 'vektor_guest_password', $password );
 	}
 	update_option( 'vektor_guest_password', $password );
+
+	/**
+	 * コマンドが実行されていなかった場合管理者宛にメールを送る
+	 */
+	$vk_theme_update = get_option( 'vk_theme_update' );
+	$vk_plugin_update = get_option( 'vk_plugin_update' );
+	$vk_updraftplus_restore = get_option( 'vk_updraftplus_restore' );
+	if ( $vk_theme_update == 'false' or $vk_plugin_update == 'false' or $vk_updraftplus_restore == 'false' ) {
+
+		$to = get_option('admin_email');
+		$subject = 'お試し申請サイト復元エラー';
+		$message = <<<EOT
+		テーマエラー：$vk_theme_update
+		プラグインエラー：$vk_plugin_update
+		復元エラー：$vk_updraftplus_restore
+EOT;
+		wp_mail( $to, $subject, $message );
+	}
 
 }
 add_action ( 'vk_trial_form_auto_cron', 'vk_trial_auto_function' );
@@ -74,7 +131,7 @@ add_filter( 'cron_schedules', 'vk_trial_interval' );
  */
 if ( !wp_next_scheduled( 'vk_trial_form_auto_cron' ) ) {
 	date_default_timezone_set( 'Asia/Tokyo' );
-	wp_schedule_event( strtotime( '2021-05-25 02:00:00' ), '300sec', 'vk_trial_form_auto_cron' );
+	wp_schedule_event( strtotime( '2021-05-27 11:40:00' ), '300sec', 'vk_trial_form_auto_cron' );
 }
 
 /**
@@ -82,28 +139,28 @@ if ( !wp_next_scheduled( 'vk_trial_form_auto_cron' ) ) {
  * 参考：https://stackoverflow.com/questions/28857693/wordpress-overwrite-contact-form-mail-2-body
  */
 function wpcf7_post_password ( $contact_form ) {
-  /**
-   * wp-cronで生成されたパスワード
-   */
-  $password = get_option( 'vektor_guest_password' );
+	/**
+	 * wp-cronで生成されたパスワード
+	 */
+	$password = get_option( 'vektor_guest_password' );
 
-  /**
-   * mailは管理者宛のメール
-   * mail_2は自動返信メール
-   */
-  $mail  = $contact_form->prop( 'mail' );
-  $mail2 = $contact_form->prop( 'mail_2' );
+	/**
+	 * mailは管理者宛のメール
+	 * mail_2は自動返信メール
+	 */
+	$mail  = $contact_form->prop( 'mail' );
+	$mail2 = $contact_form->prop( 'mail_2' );
 
-  /**
-   * メール文章のgenerate_passwordの文字列を現在の設定されているパスワードに変更する
-   */
-  $mail['body']  = str_replace( "generate_password", $password, $mail['body'] );
-  $mail2['body'] = str_replace( "generate_password", $password, $mail2['body'] );
+	/**
+	 * メール文章のgenerate_passwordの文字列を現在の設定されているパスワードに変更する
+	 */
+	$mail['body']  = str_replace( "generate_password", $password, $mail['body'] );
+	$mail2['body'] = str_replace( "generate_password", $password, $mail2['body'] );
 
-  $contact_form->set_properties( array( 'mail' => $mail ) );
-  $contact_form->set_properties( array( 'mail_2' => $mail2 ) );
+	$contact_form->set_properties( array( 'mail' => $mail ) );
+	$contact_form->set_properties( array( 'mail_2' => $mail2 ) );
 
-  return $contact_form;
+	return $contact_form;
 }
 add_action( 'wpcf7_before_send_mail', 'wpcf7_post_password' );
 
@@ -129,7 +186,7 @@ function filter_wpcf7_display_message( $message, $status ) {
 	if ( 'mail_sent_ok' == $status ) {
 		$message = "申請ありがとうございました。送信頂いたメールアドレスにログイン情報をお送りさせていただいたのでご確認お願いします";
 	} else {
-		$message = "フォームの送信に失敗しました。";
+		$message = "フォームの送信に失敗しました。しばらく時間をおいて再度お試しください。";
 	}
 	return $message;
 };
@@ -155,3 +212,20 @@ function add_admin_only_post_type_manage() {
 	);
 }
 add_action( 'init', 'add_admin_only_post_type_manage' );
+
+
+
+function my_katawara_site_container_prepend_1 () {
+  $vk_theme_update = get_option( 'vk_theme_update' );
+  $vk_plugin_update = get_option( 'vk_plugin_update' );
+  $vk_updraftplus_restore = get_option( 'vk_updraftplus_restore' );
+  var_dump($vk_theme_update);
+  var_dump($vk_plugin_update);
+  var_dump($vk_updraftplus_restore);
+  if ( $vk_theme_update == 'false' or $vk_plugin_update == 'false' or $vk_updraftplus_restore == 'false' ) {
+    echo "どれかがダメ";
+  } else {
+    echo "上手くいっている";
+  }
+}
+add_action('katawara_site-container_prepend', 'my_katawara_site_container_prepend_1');
