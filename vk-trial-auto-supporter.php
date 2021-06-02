825a1a902cb195c550432153262e9ad61455bc20
kurudrive <kurudrive@gmail.com>
2021-06-02 23:31:52 +0900

    clean: 関数に共通のプリフィックスをつける...のがWordPressの作法だったはず...（公式ディレクトリ登録プラグインはそうしないと通らない）

--- vk-trial-auto-supporter.php
+++ vk-trial-auto-supporter.php
@@ -150,7 +150,7 @@ if ( ! wp_next_scheduled( 'vk_trial_form_auto_cron' ) ) {
  * 自動返信でパスワードを送る
  * 参考：https://stackoverflow.com/questions/28857693/wordpress-overwrite-contact-form-mail-2-body
  */
-function wpcf7_post_password( $contact_form ) {
+function vktas_wpcf7_post_password( $contact_form ) {
 	/**
 	 * wp-cronで生成されたパスワード
 	 */
@@ -175,13 +175,13 @@ function wpcf7_post_password( $contact_form ) {
 
 	return $contact_form;
 }
-add_action( 'wpcf7_before_send_mail', 'wpcf7_post_password' );
+add_action( 'wpcf7_before_send_mail', 'vktas_wpcf7_post_password' );
 
 /**
  * リセット前１時間前くらいからはフォーム申請を停止する
  * https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/wp_schedule_event
  */
-function filter_wpcf7_acceptance( $true ) {
+function vktas_filter_wpcf7_acceptance( $true ) {
 	$nowdate = date_i18n( 'H' );
 	if ( 1 <= $nowdate && $nowdate < 2 ) {
 		$true = false;
@@ -190,12 +190,12 @@ function filter_wpcf7_acceptance( $true ) {
 	}
 	return $true;
 };
-add_filter( 'wpcf7_acceptance', 'filter_wpcf7_acceptance' );
+add_filter( 'wpcf7_acceptance', 'vktas_filter_wpcf7_acceptance' );
 
 /**
  * フォームの申請を止めるのでフォーム送信時のメッセージの出力を加工する
  */
-function filter_wpcf7_display_message( $message, $status ) {
+function vktas_filter_wpcf7_display_message( $message, $status ) {
 	if ( 'mail_sent_ok' == $status ) {
 		$message = '申請ありがとうございました。送信頂いたメールアドレスにログイン情報をお送りさせていただいたのでご確認お願いします';
 	} else {
@@ -203,13 +203,13 @@ function filter_wpcf7_display_message( $message, $status ) {
 	}
 	return $message;
 };
-add_filter( 'wpcf7_display_message', 'filter_wpcf7_display_message', 10, 2 );
+add_filter( 'wpcf7_display_message', 'vktas_filter_wpcf7_display_message', 10, 2 );
 
 /**
  * 管理者用投稿タイプ
  * 試用版ユーザーの申請フォームページのため
  */
-function add_admin_only_post_type_manage() {
+function vktas_add_admin_only_post_type_manage() {
 	register_post_type(
 		'vk_trial',
 		array(
@@ -224,7 +224,7 @@ function add_admin_only_post_type_manage() {
 		)
 	);
 }
-add_action( 'init', 'add_admin_only_post_type_manage' );
+add_action( 'init', 'vktas_add_admin_only_post_type_manage' );
 
 /**
  * vktas_optionsを変換
